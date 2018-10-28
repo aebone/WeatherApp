a3c9eef706d47a7e00f3307a6b2c144af3c6637f
Aline Ebone <iniebone@gmail.com>
2018-10-28 01:41:27 -0300

    Include input and button in the search bar with tests

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     render() {
         return(
-            <p>sdfdsnfio</p>
+            <form className="form-horizontal">
+                <div className="row">
+                    <div className="col-md-10">
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Type a city name"></input>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <button type="submit" className="btn btn-primary btn-block float-right">Search</button>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
---
+++ src/containers/search_bar.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import SearchBar from '../containers/search_bar';
+
+configure({ adapter: new Adapter() });
+
+const wrapper = shallow(<SearchBar />)
+
+describe('SearchBar Component', () => {
+  it('should render a input element', () => {
+    expect(wrapper.find('input').exists())
+  })
+
+  it('should render a search button', () => {
+    expect(wrapper.find('button').text()).toBe('Search');
+  })
+})
